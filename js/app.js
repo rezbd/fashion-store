4494c43ae74ee506bb06e208f4a3c7d1ceb1d0d5
rezwan <rezwan6@gmail.com>
2021-09-13 14:37:54 +0600

    added background colors

--- js/app.js
+++ js/app.js
@@ -15,9 +15,10 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
-      <h4>Avg. Ratings: ${product.rating.rate} (${product.rating.count})</h4>
+      <h5>Avg. Ratings: ${product.rating.rate}</h5>
+      <h6>(Rated by ${product.rating.count} people)</h6>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
